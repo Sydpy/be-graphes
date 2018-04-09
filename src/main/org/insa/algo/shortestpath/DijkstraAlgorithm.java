fd060ff77514ae0b6fd5c7648f996be399193fd7
sydpy <sydpy@suchmail.com>
2018-04-10 00:08:15 +0200

    Dijkstra done

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,17 +1,116 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.algo.AbstractSolution.Status;
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.algo.utils.Label;
+import org.insa.algo.utils.PriorityQueue;
+import org.insa.graph.Arc;
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+import org.insa.graph.Path;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
-    public DijkstraAlgorithm(ShortestPathData data) {
-        super(data);
-    }
+	public DijkstraAlgorithm(ShortestPathData data) {
+		super(data);
+	}
 
-    @Override
-    protected ShortestPathSolution doRun() {
-        ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
-        // TODO:
-        return solution;
-    }
+	@Override
+	protected ShortestPathSolution doRun() {
+
+		// Retrieve the graph.
+		ShortestPathData data = getInputData();
+		Graph graph = data.getGraph();
+
+		final int nbNodes = graph.size();
+
+		// Notify observers about the first event (origin processed).
+		notifyOriginProcessed(data.getOrigin());
+
+		// Initialize array of distances.
+		double[] distances = new double[nbNodes];
+		Arrays.fill(distances, Double.POSITIVE_INFINITY);
+		distances[data.getOrigin().getId()] = 0;
+
+		// Initialize array of predecessors.
+		Arc[] predecessorArcs = new Arc[nbNodes];
+
+		//Initialize PriorityQueue
+		BinaryHeap<Label<Node, Double>> queue = new BinaryHeap<>();
+		queue.insert(new Label<>(data.getOrigin(), distances[data.getOrigin().getId()]));
+
+		//Get min of our queue
+		Label<Node, Double> min;
+		double minDist;
+		Node minNode = null;
+
+		//Do this while our queue is not empty or the destination is not the min
+		while (!queue.isEmpty() && !data.getDestination().equals(minNode)) {
+
+			//Extract min from priority queue
+			min = queue.deleteMin();
+			minNode = min.value;
+			minDist = min.label;
+
+			//For each arc from the min
+			for (Arc a : minNode) {
+
+				Node dest = a.getDestination();
+
+				//Compute new distance from origin via minNode
+				double oldDist = distances[dest.getId()];
+				double newDist = minDist + data.getCost(a);
+
+				if (newDist < oldDist) {
+
+					//If old distance is Infinite, it means we just
+					//encountered this node, no need to remove it
+					if (Double.isInfinite(oldDist)) {
+						notifyNodeReached(dest);
+					} else {
+						queue.remove(new Label<>(dest, oldDist));
+					}
+
+					//Update distance and predecessor
+					distances[dest.getId()] = newDist;
+					predecessorArcs[dest.getId()] = a;
+
+					//(Re)Insert the label concerning this arc with new distance
+					queue.insert(new Label<>(dest, newDist));
+				}
+			}
+		}
+
+		ShortestPathSolution solution;
+
+		// Destination has no predecessor, the solution is infeasible...
+		if (predecessorArcs[data.getDestination().getId()] == null) {
+			solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+		} else {
+
+			// The destination has been found, notify the observers.
+			notifyDestinationReached(data.getDestination());
+
+			// Create the path from the array of predecessors...
+			ArrayList<Arc> arcs = new ArrayList<>();
+			Arc arc = predecessorArcs[data.getDestination().getId()];
+			while (arc != null) {
+				arcs.add(arc);
+				arc = predecessorArcs[arc.getOrigin().getId()];
+			}
+
+			// Reverse the path...
+			Collections.reverse(arcs);
+
+			// Create the final solution.
+			solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+		}
+
+		return solution;
+	}
 
 }
--- src/main/org/insa/algo/utils/Label.java
+++ src/main/org/insa/algo/utils/Label.java
@@ -5,8 +5,25 @@ public class Label<E, F extends Comparable<F>> implements Comparable<Label<E, F>
     public E value;
     public F label;
 
+    public Label(E value, F label) {
+        this.value = value;
+        this.label = label;
+    }
+
     @Override
     public int compareTo(Label<E, F> efLabel) {
         return label.compareTo(efLabel.label);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+
+        if (o instanceof Label) {
+            Label l = (Label) o;
+            return l.value.equals(value) && l.label.equals(label);
+        }
+
+        return false;
     }
 }
